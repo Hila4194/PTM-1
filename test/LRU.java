44df624969b771b1efc3a1ffa39f87a688b390a9
Hila4194 <hila4194@gmail.com>
2024-08-31 13:17:30 +0300

    Even-Derech 2+3

---
+++ test/LRU.java
@@ -0,0 +1,24 @@
+package test;
+
+import java.util.LinkedHashMap;
+
+public class LRU implements CacheReplacementPolicy
+{
+    private final LinkedHashMap<String, Boolean> lruMap;
+    public LRU()
+    {
+        this.lruMap = new LinkedHashMap<>(16, 0.75f, true);
+    }
+
+    @Override
+    public void add(String item)
+    {
+        lruMap.put(item, true); 
+    }
+
+    @Override
+    public String remove()
+    {
+        return lruMap.keySet().iterator().next();
+    }
+}
